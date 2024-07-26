bcd0e100d87aebe8160eb75d3eed0a48748e55e6
Ruben Carreira <r.carreira@kipt000958.home>
2024-07-26 20:38:38 +0100

    fix: implement grid

--- src/components/MatchList.tsx
+++ src/components/MatchList.tsx
@@ -14,6 +14,8 @@ const MatchList: React.FC<MatchListProps> = ({ matches }) => {
     return dateA.getTime() - dateB.getTime();
   });
 
+  console.log(sortedMatches.length);
+
   // Group matches into columns of 4
   const columns = [];
   for (let i = 0; i < sortedMatches.length; i += 4) {
@@ -21,26 +23,17 @@ const MatchList: React.FC<MatchListProps> = ({ matches }) => {
   }
 
   return (
-    <div
-      className={`match-list-container ${
-        columns.length > 1 ? "hasColumns" : ""
-      }`}
-    >
-      {columns.map((column, columnIndex) => (
-        <div key={columnIndex} className="match-column">
-          {column.map((match) => (
-            <Match
-              key={match.date + match.time + match.teamName}
-              small={columns.length > 1}
-              day={new Date(match.date).getDate()}
-              team_name={match.teamName}
-              time={match.time}
-              extraInfo={match.leagueName}
-              teamLogo={match.teamIcon}
-              field={match.field}
-            />
-          ))}
-        </div>
+    <div className={`match-list-container-${sortedMatches.length > 4 ? "2" : "1"}`}>
+      {sortedMatches.map((match) => (
+        <Match
+          key={match.date + match.time + match.teamName}
+          day={new Date(match.date).getDate()}
+          team_name={match.teamName}
+          time={match.time}
+          extraInfo={match.leagueName}
+          teamLogo={match.teamIcon}
+          field={match.field}
+        />
       ))}
     </div>
   );
