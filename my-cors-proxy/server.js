feb13f4f37c14e9024e1479721f5941ef18703e4
Miguel Félix <poke.man.2006@gmail.com>
2024-07-27 13:52:40 +0100

    added local cors server

---
+++ my-cors-proxy/server.js
@@ -0,0 +1,14 @@
+const cors_proxy = require("cors-anywhere");
+
+const host = "0.0.0.0";
+const port = 8080;
+
+cors_proxy
+  .createServer({
+    originWhitelist: [], // Allow all origins
+    requireHeader: ["origin", "x-requested-with"],
+    removeHeaders: ["cookie", "cookie2"],
+  })
+  .listen(port, host, () => {
+    console.log(`Running CORS Anywhere on ${host}:${port}`);
+  });
